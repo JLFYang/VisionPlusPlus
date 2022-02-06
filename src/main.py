d1714eef7a57b79147c287e92ec3b7d56f3406d9
Jacob Yang <yangjl@pnw.edu>
2022-02-06 15:59:00 -0600

    Created a Template for Starting the Project

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# Group 3: Daniel Gertz, Jacob Yang, Patrick Marsh, Mikey Plaut, Zach Hillman
+# Vision++ : Simple Video Editing Software
+
+import os
+from moviepy.editor import *
+import tkinter as tk
+from tkinter import filedialog
+
