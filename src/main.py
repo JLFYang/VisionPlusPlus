55aeeb554f36316379aacc33a15dc6837709cd76
Jacob Yang <yangjl@pnw.edu>
2022-02-07 16:15:15 -0600

    started a file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 # Group 3: Daniel Gertz, Jacob Yang, Patrick Marsh, Mikey Plaut, Zach Hillman
 # Vision++ : Simple Video Editing Software
 
-import os
-from moviepy.editor import *
 import tkinter as tk
-from tkinter import filedialog
 
